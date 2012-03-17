339beea1473d361b7170e2256f931f418670f18e
sillypeng <sillypeng@gmail.com>
2012-03-17 23:14:26 +0100

    commit all

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-//@author: Zipeng WU
+/* author: Zipeng WU */
 var clipList = new Array("33348669","33270207","25123854","14959205","5388651","6864953","6588265","9648208","5503825");
 var videoList = new Array("33348669-LEPARC2BLAIRWITCHENDING","33270207-LEPARC2HAPPYENDING","25123854-LESNIPER","110504-GAMEONE","110504-HORDES","110504-BETCLIC","110504-MORGAN1","110504-MORGAN2","110504-BLYG");
 var clientList = new Array("wandaproduction","auditoire","freshresearch","lachose","lestelecreateurs","profirst","tbwa","spoke");
@@ -15,8 +15,13 @@ jQuery.fn.center = function () {
 	return this;
 }
 jQuery.preloadImages = function(list) {
-	for(var i = 0; i<list.length; i++) {
-		jQuery("<img>").attr("src", list[i]);
+	if(list instanceof Array){
+		for(var i = 0; i<list.length; i++) {
+			jQuery("<img>").attr("src", list[i]);
+		}
+	}
+	else{
+		jQuery("<img>").attr("src", list);
 	}
 }
 
@@ -26,65 +31,29 @@ var constructClip = function(str, width, height){
 
 var sniperv=constructClip("25123854","800","450");
 
-var getVideoList = function(list){
-	if(list.length == null){
-		return "img/video/"+ list +".png";
-	}
-	else{
-		var videoList = new Array();
-		for(var i=0; i<list.length; i++){
-			videoList[i] = "img/video/"+ list[i] +".png";
-		}
-		return videoList;
-	}
-}
-
-var getVideoListOver = function(list){
-	if(list.length == null){
-		return "img/video/"+ list +"-over.png";
-	}
-	else{
-		var videoList = new Array();
-		for(var i=0; i<list.length; i++){
-			videoList[i] = "img/video/"+ list[i] +"-over.png";
-		}
-		return videoList;
-	}
-}
-
-var getImgList = function(list){
-	if(list.length == null){
-		return "img/"+ list +".png";
-	}
-	else{
+//transform an image or an array of images
+var getImgPath = function(list, isVideo, isHover){
+	var dirPath = isVideo? "img/video/": "img/";
+	var subfix = isHover? "-over.png" : ".png";
+	if(list instanceof Array){
 		var imgList = new Array();
 		for(var i=0; i<list.length; i++){
-			imgList[i] = "img/"+ list[i] +".png";
+			imgList[i] = dirPath + list[i] + subfix;
 		}
 		return imgList;
 	}
+	else{
+		return dirPath + list + subfix;
+	}
 }
 
-var getImgListOver = function(list){
-	if(list.length == null){
-		return "img/"+ list +"-over.png";
-	}
-	else{
-		var imgList = new Array();
-		for(var i=0; i<list.length; i++){
-			imgList[i] = "img/"+ list[i] +"-over.png";
-		}
-		return imgList;
-	}
-}
 
 
 $(document).ready( function() {
-//	$.preloadImages("img/information-gris.png","img/work-gris.png","img/video/33270207-LEPARC2HAPPYENDINGnoir.png","img/video/25123854-LESNIPERnoir.png", "img/video/33348669-LEPARC2BLAIRWITCHENDINGnoir.png", "img/video/sniperclip.png","img/tbwa-coul.png","img/profirst-coul.png","img/send-gris.png","img/lestelecreateurs-coul.png","img/lachose-coul.png","img/freshresearch-coul.png","img/auditoire-coul.png","img/wandaproduction-coul.png","img/video/110504-BETCLIC.png","img/video/110504-BETCLIC-noir.png","img/video/110504-BLYG.png","img/video/110504-BLYG-noir.png","img/video/110504-GAMEONE.png","img/video/110504-GAMEONE-noir.png","img/video/110504-HORDES-noir.png","img/video/110504-HORDES.png","img/video/110504-MORGAN1-noir.png","img/video/110504-MORGAN1.png","img/video/110504-MORGAN2-noir.png","img/video/110504-MORGAN2.png","img/spoke-coul.png");
-	$.preloadImages(getImgListOver(otherImgList));
-	$.preloadImages(getVideoListOver(videoList));
-	$.preloadImages(getImgListOver(clientList));
-	
+	$.preloadImages(getImgPath(otherImgList,false,true));
+	$.preloadImages(getImgPath(videoList,true,true));
+	$.preloadImages(getImgPath(clientList,false,true));
+	$.preloadImages(getImgPath("sniperclip",true,true));
 	
 	$(".video").click( function(e) {
 		$("#background").css({
@@ -97,12 +66,10 @@ $(document).ready( function() {
 			if($(this).attr("id")==video) {
 				$("#videoclip").html(constructClip(clipList[i-1],"640","360"));
 			}
-			
 		}
 		$("#videoclip")
 		.center()
 		.fadeIn("slow");
-		return false;
 	});
 	$(document).keypress( function(e) {
 		if(e.keyCode==27) {
@@ -113,8 +80,8 @@ $(document).ready( function() {
 	
 	$(".video").hover( function() {
 		for(var i = 1; i<=videoList.length; i ++){
-			if($(this).id == "video" + i){
-				$(this).attr("src",getVideoListOver(videoList[i-1]));
+			if($(this).attr("id") == "video" + i){
+				$(this).attr("src",getImgPath(videoList[i-1],true,true));
 				break;
 			}
 		}
@@ -122,8 +89,26 @@ $(document).ready( function() {
 	});
 	$(".video").mouseout( function() {
 		for(var i = 1; i<=videoList.length; i ++){
-			if($(this).id == "video" + i){
-				$(this).attr("src",getVideoList(videoList[i-1]));
+			if($(this).attr("id") == "video" + i){
+				$(this).attr("src",getImgPath(videoList[i-1],true,false));
+				break;
+			}
+		}
+	});
+	
+	$(".clientClickMap").hover( function() {
+		for(var i = 1; i<=clientList.length; i ++){
+			if($(this).attr("id") == "clientmap" + i){
+				$("#client" + i).attr("src",getImgPath(clientList[i-1],false,true));
+				break;
+			}
+		}
+		
+	});
+	$(".clientClickMap").mouseout( function() {
+		for(var i = 1; i<=clientList.length; i ++){
+			if($(this).attr("id") == "clientmap" + i){
+				$("#client" + i).attr("src",getImgPath(clientList[i-1],false,false));
 				break;
 			}
 		}
@@ -339,114 +324,4 @@ $(document).ready( function() {
 	$("#img_sniper").mouseout( function() {
 		$("#img_sniper").attr("src","img/video/sniperclip.png");
 	});
-	
-	/*
-	"33348669-LEPARC2BLAIRWITCHENDING","33270207-LEPARC2HAPPYENDING","25123854-LESNIPER","110504-GAMEONE","110504-HORDES","110504-BETCLIC","110504-MORGAN1","110504-MORGAN2","110504-BLYG"
-	
-	$("#video1").hover( function() {
-		$("#video1").attr("src","img/video/33348669-LEPARC2BLAIRWITCHENDINGnoir.png");
-	});
-	$("#video1").mouseout( function() {
-		$("#video1").attr("src","img/video/33348669-LEPARC2BLAIRWITCHENDING.png");
-	});
-	$("#video2").hover( function() {
-		$("#video2").attr("src","img/video/33270207-LEPARC2HAPPYENDINGnoir.png");
-	});
-	$("#video2").mouseout( function() {
-		$("#video2").attr("src","img/video/33270207-LEPARC2HAPPYENDING.png");
-	});
-	$("#video3").hover( function() {
-		$("#video3").attr("src","img/video/25123854-LESNIPERnoir.png");
-	});
-	$("#video3").mouseout( function() {
-		$("#video3").attr("src","img/video/25123854-LESNIPER.png");
-	});
-	$("#video6").hover( function() {
-		$("#video6").attr("src","img/video/110504-BETCLIC-noir.png");
-	});
-	$("#video6").mouseout( function() {
-		$("#video6").attr("src","img/video/110504-BETCLIC.png");
-	});
-	$("#video9").hover( function() {
-		$("#video9").attr("src","img/video/110504-BLYG-noir.png");
-	});
-	$("#video9").mouseout( function() {
-		$("#video9").attr("src","img/video/110504-BLYG.png");
-	});
-	$("#video4").hover( function() {
-		$("#video4").attr("src","img/video/110504-GAMEONE-noir.png");
-	});
-	$("#video4").mouseout( function() {
-		$("#video4").attr("src","img/video/110504-GAMEONE.png");
-	});
-	$("#video5").hover( function() {
-		$("#video5").attr("src","img/video/110504-HORDES-noir.png");
-	});
-	$("#video5").mouseout( function() {
-		$("#video5").attr("src","img/video/110504-HORDES.png");
-	});
-	$("#video7").hover( function() {
-		$("#video7").attr("src","img/video/110504-MORGAN1-noir.png");
-	});
-	$("#video7").mouseout( function() {
-		$("#video7").attr("src","img/video/110504-MORGAN1.png");
-	});
-	$("#video8").hover( function() {
-		$("#video8").attr("src","img/video/110504-MORGAN2-noir.png");
-	});
-	$("#video8").mouseout( function() {
-		$("#video8").attr("src","img/video/110504-MORGAN2.png");
-	});
-	
-	"wandaproduction","auditoire","freshresearch","lachose","lestelecreateurs","profirst","tbwa","spoke"
-	
-	$("#clientmap1").hover( function() {
-		$("#client1").attr("src","img/wandaproduction-coul.png");
-	});
-	$("#clientmap1").mouseout( function() {
-		$("#client1").attr("src","img/wandaproduction-nb.png");
-	});
-	$("#clientmap2").hover( function() {
-		$("#client2").attr("src","img/auditoire-coul.png" );
-	});
-	$("#clientmap2").mouseout( function() {
-		$("#client2").attr("src","img/auditoire-nb.png" );
-	});
-	$("#clientmap3").hover( function() {
-		$("#client3").attr("src","img/freshresearch-coul.png");
-	});
-	$("#clientmap3").mouseout( function() {
-		$("#client3").attr("src","img/freshresearch-nb.png");
-	});
-	$("#clientmap4").hover( function() {
-		$("#client4").attr("src","img/lachose-coul.png");
-	});
-	$("#clientmap4").mouseout( function() {
-		$("#client4").attr("src","img/lachose-nb.png");
-	});
-	$("#clientmap5").hover( function() {
-		$("#client5").attr("src","img/lestelecreateurs-coul.png");
-	});
-	$("#clientmap5").mouseout( function() {
-		$("#client5").attr("src","img/lestelecreateurs-nb.png");
-	});
-	$("#clientmap6").hover( function() {
-		$("#client6").attr("src","img/profirst-coul.png");
-	});
-	$("#clientmap6").mouseout( function() {
-		$("#client6").attr("src","img/profirst-nb.png");
-	});
-	$("#clientmap7").hover( function() {
-		$("#client7").attr("src","img/tbwa-coul.png");
-	});
-	$("#clientmap7").mouseout( function() {
-		$("#client7").attr("src","img/tbwa-nb.png");
-	});
-	$("#clientmap8").hover( function() {
-		$("#client8").attr("src","img/spoke-coul.png");
-	});
-	$("#clientmap8").mouseout( function() {
-		$("#client8").attr("src","img/spoke-nb.png");
-	});
-	*/
 });
